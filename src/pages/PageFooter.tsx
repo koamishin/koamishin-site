beb9f7a470ae739c413eb6d96b459b5700d5c3fe
Luis <marianolouis18@gmail.com>
2025-07-29 21:30:54 +0800

    changed phone

--- src/pages/PageFooter.tsx
+++ src/pages/PageFooter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Phone, Mail, MapPin, Facebook, Instagram } from "lucide-react";
+import {  Mail, MapPin, Facebook, Instagram } from "lucide-react";
 
 const PageFooter: React.FC = () => {
   return (
