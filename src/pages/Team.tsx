7aab0494b19865198eed4c383189fcd294eac6cc
Yuka <128908897+yukazakiri@users.noreply.github.com>
2026-06-02 21:48:16 +0800

    Show team members in one row

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -220,7 +220,7 @@ const Team: React.FC = () => {
             </p>
           </header>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 xl:gap-8">
             {artisans.map((artisan, index) => (
               <div
                 key={artisan.id}
